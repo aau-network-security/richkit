94e4a0d2ee7360ec91257e63b0b20eb9203b3dbd
Egon Kidmose <egk@es.aau.dk>
2019-09-24 12:51:13 +0200

    Ideas in comments moved to Issue #4

--- dat/retrieve/__init__.py
+++ dat/retrieve/__init__.py
@@ -4,15 +4,6 @@ This module provides the ability to retrieve data on domain names of
 any sort. It comes without the "confidentiality contract" of
 `dat.lookup`.
 
-Ideas for additiaon sources:
-
- * urlvoid.com (all of the 36 services checked?)
- * Virus total
- * WHOIS (suggestion: pywhois)
- * github.com/aau-network-security/kraaler
- * Services on top of HTTPS Certificate Transparency logs
- * https://www.lucidchart.com/documents/edit/e1fc4013-d4db-41d5-ba47-01ac9ae19fa0/0_0
-
 """
 
 __all__=['symantec']
