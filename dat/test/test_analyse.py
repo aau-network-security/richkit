700ffb8f76b7bec275728d924b37f82a7a44d851
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2019-10-10 23:28:28 +0200

    whois library has been added into requirements.txt file

--- dat/test/test_analyse.py
+++ dat/test/test_analyse.py
@@ -13,24 +13,23 @@ from dat.analyse.analyse import get_ratio_vowels_2ld
 from dat.analyse.analyse import get_ratio_special_2ld
 from dat.analyse.analyse import get_ratio_consonants_2ld
 
+
 class TestAnalyse(unittest.TestCase):
 	def setUp(self):
-		self.domain='www.google.co.uk'
+		self.domain = 'www.google.co.uk'
 
 	def test_get_domain_name_features(self):
-		domain_features  = get_domain_name_features(self.domain)
+		domain_features = get_domain_name_features(self.domain)
 		if (domain_features['num_tokens'] == '4') and \
-				(domain_features['len2ld']== '6') and \
-				(domain_features['len_domain']=='13'):
-			self.assertEqual(True,True)
+				(domain_features['len2ld'] == '6') and \
+				(domain_features['len_domain'] == '13'):
+			self.assertEqual(True, True)
 		else:
-			self.assertEqual(True,False)
-
+			self.assertEqual(True, False)
 
 	def test_get_tld(self):
 		domain_tld = get_tld(self.domain)
 		assert domain_tld == "uk"
-
 
 	def test_get_language(self):
 		language = get_language(self.domain)
@@ -47,21 +46,24 @@ class TestAnalyse(unittest.TestCase):
 
 	@unittest.skip("Skipping dict test since no data folder  ")
 	def test_get_grams_dict_2ld(self):
-		grams_dict_2ld  = get_grams_dict_2ld(self.domain)
+		grams_dict_2ld = get_grams_dict_2ld(self.domain)
 		assert grams_dict_2ld == '25.77346214958408'
 
-	def test_get_num_words_2ld(self,domain = 'www.google.co.uk'):
+	def test_get_num_words_2ld(self, domain='www.google.co.uk'):
 		num_words_2ld = get_num_words_2ld(domain)
-		assert  num_words_2ld =='0'
+		assert num_words_2ld == '0'
 
 	def test_get_num_of_vowels_2ld(self):
 		vowels = get_num_of_vowels_2ld(self.domain)
-		assert  vowels == '3'
+		assert vowels == '3'
+
 	def test_get_ratio_vowels_2ld(self):
-		ratio_vowels  = get_ratio_vowels_2ld(self.domain)
+		ratio_vowels = get_ratio_vowels_2ld(self.domain)
 		assert ratio_vowels == '0.5'
+
 	def test_get_num_of_consonants_2ld(self):
 		assert get_num_of_consonants_2ld(self.domain) == '3'
+
 	def test_get_ratio_consonants_2ld(self):
 		assert get_ratio_consonants_2ld(self.domain) == '0.5'
 
