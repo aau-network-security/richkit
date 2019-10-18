6c0ed71f6619f8fe3e07ec496d09cdf55314ca0b
gianmarcomennecozzi <gianmarco.mennecozzi@gmail.com>
2019-10-18 15:58:28 +0200

    Fixed Lookup/Geo functions. Deleted old files

--- dat/test/test_retrieve.py
+++ dat/test/test_retrieve.py
@@ -1,12 +1,23 @@
 import os.path
 import unittest
+from dat.retrieve import dns
 from dat.retrieve.symantec import fetch_from_internet
 from dat.retrieve.symantec import fetch_categories
 from dat.retrieve.symantec import load_categories
 from dat.retrieve.symantec import categories_url
-from dat.retrieve import dns
+
 
 class RetrieveTestCase(unittest.TestCase):
+
+    def test_dns_a(self):
+        a_record = dns.get_a_record("amazon.co.uk")
+        a_list = ["54.239.33.58", "54.239.34.171", "178.236.7.220"]
+        assert len(a_record) == len(a_list) and sorted(a_record) == sorted(a_list)
+
+    def test_dns_ptr(self):
+        ptr_record = dns.get_ptr_record("8.8.4.4")
+        ptr_list = ["dns.google."]
+        assert len(ptr_record) == len(ptr_list) and sorted(ptr_record) == sorted(ptr_list)
 
     def test_fetch_categories(self, categories_file_path='categories_list.txt'):
         ## make sure that categories url is accessible and fetched correctly
@@ -31,16 +42,6 @@ class RetrieveTestCase(unittest.TestCase):
         if os.path.isfile(categories_file_path):
             os.remove(categories_file_path)
         os.remove(categorized_url_path)
-
-    def test_dns_a(self):
-        a_record = dns.get_a_record("amazon.co.uk")
-        a_list = ["54.239.33.58", "54.239.34.171", "178.236.7.220"]
-        assert len(a_record) == len(a_list) and sorted(a_record) == sorted(a_list)
-
-    def test_dns_ptr(self):
-        ptr_record = dns.get_ptr_record("8.8.4.4")
-        ptr_list = ["dns.google."]
-        assert len(ptr_record) == len(ptr_list) and sorted(ptr_record) == sorted(ptr_list)
 
 if __name__ == '__main__':
     unittest.main()
