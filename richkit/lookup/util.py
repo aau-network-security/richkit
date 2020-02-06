31ad0bc2b7b4e582c9aa963988896e7000fe0e4f
Egon Kidmose <egk@es.aau.dk>
2020-02-06 11:10:42 +0100

    fix for lookup.geo.MaxMind_CC_DB: Use license key

--- richkit/lookup/util.py
+++ richkit/lookup/util.py
@@ -6,6 +6,20 @@ import logging
 logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     level=logging.DEBUG)
+"""
+Lookups in the MaxMind GeoLite2 databases.
+
+A credentials are required as per [#GeoLite2_CCPA_GDPR]_:
+
+#. Sign up for a MaxMind account (no purchase required): https://www.maxmind.com/en/geolite2/signup
+#. Set your password and create a license key: https://www.maxmind.com/en/accounts/current/license-key
+#. Setup your download mechanism by using our GeoIP Update program or creating a direct download script: https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads
+
+.. rubric:: Footnotes
+
+.. [#GeoLite2_CCPA_GDPR] https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
+"""
+
 logger = logging.getLogger(__name__)
 temp_directory = tempfile.mkdtemp()
 
@@ -16,10 +30,15 @@ class MaxMind_CC_DB(object):
     Country database provided by MaxMind
 
     """
-    MASTERURL = "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
+    MASTERURL = ( # https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads
+        "https://download.maxmind.com/app/geoip_download?"
+        "edition_id=GeoLite2-Country&"
+        "license_key={license_key}&"
+        "suffix=tar.gz"
+    ).format(
+        license_key=os.environ['MAXMIND_LICENSE_KEY'],
+    )
     MASTERFILE = temp_directory + "/country.tar.gz"
-
-
 
 
     @classmethod
@@ -28,13 +47,26 @@ class MaxMind_CC_DB(object):
         Download the Country database in zip format from the MaxMind website, then extract it
 
         """
-        logger.info("Downloading MaxMind_CC_DB... ")
-        response = requests.get(cls.MASTERURL, stream=True)
+        logger.debug("Downloading MaxMind_CC_DB... ")
+        try:
+            response = requests.get(cls.MASTERURL, stream=True)
+        except Exception as e:
+            logger.error('Reraising Exception raised by requests.get ({})'.format(e))
+            raise e
+
         if response.status_code == 200:
             with open(cls.MASTERFILE, 'wb') as file:
                 file.write(response.content)
         else:
-            logger.error('Error while downloading the Country DB ...')
+            msg = (
+                'Error while downloading the Country DB '
+                '(Status Code={}): {}'
+            ).format(
+                response.status_code,
+                response.text,
+            )
+            logger.error(msg)
+            raise Exception(msg)
 
         if os.path.exists(cls.MASTERFILE):
             subprocess.Popen(['tar', '-xzf', cls.MASTERFILE], cwd=temp_directory)
