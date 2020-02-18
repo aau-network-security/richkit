3fa2864b56b4aaf232163736d7b71a625dc7950e
kidmose <kidmose@users.noreply.github.com>
2020-02-18 07:40:33 +0100

    Feature/fix lookup.country and lookup.asn #54 (#55)
    * test for lookup.geo.MaxMind_CC_DB
    
    * fix for lookup.geo.MaxMind_CC_DB: Use license key
    
    * minor errors
    
    * learning to CI with GH Actions; secrets in env
    
    * Fix ASN like Country was fixed in previous commits
    
    * MAXMIND_LICENSE_KEY also available for "coverage" step in GH actions
    
    * Replace hard coded posix paths ('/') with pathlib.Path

--- richkit/lookup/util.py
+++ richkit/lookup/util.py
@@ -3,9 +3,25 @@ import os, subprocess
 import time, calendar, shutil
 import tempfile
 import logging
+from pathlib import Path
+
 logging.basicConfig(format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
                     datefmt='%m-%d %H:%M',
                     level=logging.DEBUG)
+"""
+Lookups in the MaxMind GeoLite2 databases.
+
+A license key is required as per [#GeoLite2_CCPA_GDPR]_:
+
+#. Sign up for a MaxMind account (no purchase required): https://www.maxmind.com/en/geolite2/signup
+#. Set your password and create a license key: https://www.maxmind.com/en/accounts/current/license-key
+#. Setup your download mechanism by using our GeoIP Update program or creating a direct download script: https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads
+
+.. rubric:: Footnotes
+
+.. [#GeoLite2_CCPA_GDPR] https://blog.maxmind.com/2019/12/18/significant-changes-to-accessing-and-using-geolite2-databases/
+"""
+
 logger = logging.getLogger(__name__)
 temp_directory = tempfile.mkdtemp()
 
@@ -16,10 +32,15 @@ class MaxMind_CC_DB(object):
     Country database provided by MaxMind
 
     """
-    MASTERURL = "https://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.tar.gz"
-    MASTERFILE = temp_directory + "/country.tar.gz"
-
-
+    MASTERURL = ( # https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads
+        "https://download.maxmind.com/app/geoip_download?"
+        "edition_id=GeoLite2-Country&"
+        "license_key={license_key}&"
+        "suffix=tar.gz"
+    ).format(
+        license_key=os.environ['MAXMIND_LICENSE_KEY'],
+    )
+    MASTERFILE = str(Path(temp_directory, "country.tar.gz"))
 
 
     @classmethod
@@ -28,13 +49,26 @@ class MaxMind_CC_DB(object):
         Download the Country database in zip format from the MaxMind website, then extract it
 
         """
-        logger.info("Downloading MaxMind_CC_DB... ")
-        response = requests.get(cls.MASTERURL, stream=True)
+        logger.debug("Downloading MaxMind_CC_DB... ")
+        try:
+            response = requests.get(cls.MASTERURL, stream=True)
+        except Exception as e:
+            logger.error('Reraising Exception raised by requests.get ({})'.format(e))
+            raise e
+
         if response.status_code == 200:
             with open(cls.MASTERFILE, 'wb') as file:
                 file.write(response.content)
         else:
-            logger.error('Error while downloading the Country DB ...')
+            msg = (
+                'Error while downloading the Country DB '
+                '(Status Code={}): {}'
+            ).format(
+                response.status_code,
+                response.text,
+            )
+            logger.error(msg)
+            raise Exception(msg)
 
         if os.path.exists(cls.MASTERFILE):
             subprocess.Popen(['tar', '-xzf', cls.MASTERFILE], cwd=temp_directory)
@@ -67,7 +101,11 @@ class MaxMind_CC_DB(object):
         filtered_dir = [x for x in os.listdir(self.path_db) if x.startswith('GeoLite2-Country_')]
         sorted_dir = sorted(filtered_dir, reverse=True)
         if sorted_dir:
-            return temp_directory + "/" + sorted_dir[0] + "/GeoLite2-Country.mmdb"
+            return str(Path(
+                temp_directory,
+                sorted_dir[0],
+                "GeoLite2-Country.mmdb",
+            ))
         else:
             return None
 
@@ -79,8 +117,15 @@ class MaxMind_ASN_DB():
 
     """
 
-    MASTERURL = "https://geolite.maxmind.com/download/geoip/database/GeoLite2-ASN.tar.gz"
-    MASTERFILE = temp_directory + "/asn.tar.gz"
+    MASTERURL = ( # https://dev.maxmind.com/geoip/geoipupdate/#Direct_Downloads
+        "https://download.maxmind.com/app/geoip_download?"
+        "edition_id=GeoLite2-ASN&"
+        "license_key={license_key}&"
+        "suffix=tar.gz"
+    ).format(
+        license_key=os.environ['MAXMIND_LICENSE_KEY'],
+    )
+    MASTERFILE = str(Path(temp_directory, "asn.tar.gz"))
 
     @classmethod
     def get_db(cls):
@@ -88,13 +133,26 @@ class MaxMind_ASN_DB():
         Download the Country database in zip format from the MaxMind website, then extract it
 
         """
-        logger.info('Downloading the ASN DB ... ')
-        response = requests.get(cls.MASTERURL, stream=True)
+        logger.debug('Downloading the ASN DB ... ')
+        try:
+            response = requests.get(cls.MASTERURL, stream=True)
+        except Exception as e:
+            logger.error('Reraising Exception raised by requests.get ({})'.format(e))
+            raise e
+
         if response.status_code == 200:
             with open(cls.MASTERFILE, 'wb') as file:
                 file.write(response.content)
         else:
-            logger.error('Error while downloading the ASN DB ....')
+            msg = (
+                'Error while downloading the ASN DB '
+                '(Status Code={}): {}'
+            ).format(
+                response.status_code,
+                response.text,
+            )
+            logger.error(msg)
+            raise Exception(msg)
 
         if os.path.exists(cls.MASTERFILE):
             subprocess.Popen(['tar', '-xzf', cls.MASTERFILE], cwd=temp_directory)
@@ -127,6 +185,10 @@ class MaxMind_ASN_DB():
         filtered_dir = [x for x in os.listdir(self.path_db) if x.startswith('GeoLite2-ASN_')]
         sorted_dir = sorted(filtered_dir, reverse=True)
         if sorted_dir:
-            return temp_directory + "/" + sorted_dir[0] + "/GeoLite2-ASN.mmdb"
+            return str(Path(
+                temp_directory,
+                sorted_dir[0],
+                "GeoLite2-ASN.mmdb",
+            ))
         else:
             return None
