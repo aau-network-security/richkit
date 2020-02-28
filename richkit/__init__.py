a9eba046c4fb8dcf9a0b8175c0b43551e538c183
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-02-28 11:54:41 +0100

    docs updated for readdocs

--- richkit/__init__.py
+++ richkit/__init__.py
@@ -1,7 +1,7 @@
-"""dat is the Domain Analysis Toolkit
+"""richkit is the domain enrichment kit
 
 See the `README
-<https://github.com/aau-network-security/domain-analysis-toolkit/blob/master/README.md>`_
+<https://github.com/aau-network-security/richkit/blob/master/README.md>`_
 for a general introduction.
 
 """
