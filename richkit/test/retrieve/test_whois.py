042795e43f0d74bf20e34c1178c6b1618951b906
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-03-24 19:01:07 +0300

    skipped test activated

--- richkit/test/retrieve/test_whois.py
+++ richkit/test/retrieve/test_whois.py
@@ -1,10 +1,22 @@
 import unittest
+from datetime import datetime
+from richkit.retrieve import whois
 
 
 class URLVoidTestCase(unittest.TestCase):
-    @unittest.skip("No test has been implemented")
-    def test_nothing(self):
-        self.fail("No test has been implemented")
+    def setUp(self):
+        self.domains = ["www.google.com", "www.cloudflare.com"]
+
+    # .dk domains give unknownTld exception !
+
+    def test_info(self):
+        # last updated field skipped since it could be None
+        for i in self.domains:
+            whois_info = whois.get_whois_info(i)
+            date = whois_info["d_expiration_date"]
+            registrar = whois_info["d_registrar"]
+            assert len(registrar) > 0
+            assert type(date) == datetime
 
 
 if __name__ == '__main__':
