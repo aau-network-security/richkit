d25a893a19e11d2ba835b4bbf160de4c9b3d118a
Ahmet Turkmen <f.ahmet.turkmen@icloud.com>
2020-04-02 16:57:10 +0300

    check env_variable, pring warning to user

--- richkit/lookup/geo.py
+++ richkit/lookup/geo.py
@@ -2,6 +2,24 @@ from richkit.lookup.util import MaxMindDB
 import os
 
 
+def get_license_key(license_key='MAXMIND_LICENSE_KEY'):
+    """
+    @param license_key: Name of environment variable
+    @return: license of MaxMindDB from environent variables as string
+    @return: in case of error, returns Exception, more specifically KeyError
+    """
+    try:
+        maxmind_db_license = os.environ[license_key]
+        return maxmind_db_license
+    except Exception:
+        print("\nWARNING: No MAXMIND LICENSE KEY Found in environment variables")
+        print("\nUsage of lookup module might be affected due to no MaxMind DB License".strip())
+        print("\nMore info ? Check here: https://github.com/aau-network-security/richkit/wiki/Retrieve-and-configure"
+              "-licence-key".strip())
+        print("Proceeding anyway...")
+        return 'NOLICENSEKEYFOUND'
+
+
 def get_country(ip_address):
     """
     Return the country code of a given IP address
@@ -9,6 +27,7 @@ def get_country(ip_address):
     :param ip_address: IP Address (string)
 
     """
+
     try:
         country_code_db = MaxMindDB((
             "https://download.maxmind.com/app/geoip_download?"
