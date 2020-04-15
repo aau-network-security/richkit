7a4a8edfd8b75f15d895d8fe61f5ab1e0cc0f904
Egon Kidmose <egk@es.aau.dk>
2020-04-15 15:05:27 +0200

    Handle Thin WHOIS; There **MAY** be two expirations for e.g. .com

--- richkit/test/retrieve/test_whois.py
+++ richkit/test/retrieve/test_whois.py
@@ -13,13 +13,19 @@ class WhoisTestCase(unittest.TestCase):
         d = "www.google.com"
         w = whois.get_whois_info(d)
         self.assertTrue(len(w['registrar']) > 0)
-        self.assertIsInstance(w['expiration_date'], datetime)
+        # .com uses "thin" WHOIS, so we get expiry from both registry
+        # and registrar;
+        self.assertTrue(len(w['expiration_date']) == 2)
+        self.assertIsInstance(w['expiration_date'][0], datetime)
+        self.assertIsInstance(w['expiration_date'][1], datetime)
 
         d = "www.cloudflare.com"
         w = whois.get_whois_info(d)
         self.assertTrue('registrar' in w)
         self.assertTrue(len(w['registrar']) > 0)
         self.assertTrue('expiration_date' in w)
+        # .com uses "thin" WHOIS, so we get expiry from both registry
+        # and registrar, but they are equal here, so only one is returned;
         self.assertIsInstance(w['expiration_date'], datetime)
 
 
