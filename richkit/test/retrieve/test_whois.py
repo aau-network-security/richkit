ecc6e883afaede4349b8c7218c6af84adf2739f3
Egon Kidmose <egk@es.aau.dk>
2020-04-15 14:37:13 +0200

    Restructure `test_whois.py` for more test (No change in tests here).

--- richkit/test/retrieve/test_whois.py
+++ richkit/test/retrieve/test_whois.py
@@ -1,23 +1,26 @@
 import unittest
 from datetime import datetime
-from richkit.retrieve import whois as wh
+from richkit.retrieve import whois
 
 
 class WhoisTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.domains = ["www.google.com", "www.cloudflare.com"]
 
     # .dk domains give unknownTld exception !
     @unittest.skip("No reason to skip however it fails on ci requires more investigation")
     def test_get_whois_info(self):
         # last updated field skipped since it could be None
-        for i in self.domains:
-            whois_info = wh.get_whois_info(i)
-            date = whois_info["d_expiration_date"]
-            registrar = whois_info["d_registrar"]
-            assert len(registrar) > 0
-            assert type(date) == datetime
+
+        d = "www.google.com"
+        w = whois.get_whois_info(d)
+        self.assertTrue(len(w['registrar']) > 0)
+        self.assertIsInstance(w['expiration_date'], datetime)
+
+        d = "www.cloudflare.com"
+        w = whois.get_whois_info(d)
+        self.assertTrue('registrar' in w)
+        self.assertTrue(len(w['registrar']) > 0)
+        self.assertTrue('expiration_date' in w)
+        self.assertIsInstance(w['expiration_date'], datetime)
 
 
 if __name__ == '__main__':
