e641543738f073d402ebc1e4eea25ac3fae8b577
kidmose <kidmose@users.noreply.github.com>
2020-04-21 11:15:26 +0200

    Feature/python whois #110 (#126)
    * Going from whois to python-whois
    
    * Use a py2-less python-whois, until upstream python-whois is fixed
    
    Cf. Fixes richardpenman/pywhois#38
    
    * Use pywhois fork what also has 'SyntaxWarning`s fixed
    
    Solves #110

--- richkit/test/retrieve/test_whois.py
+++ richkit/test/retrieve/test_whois.py
@@ -1,23 +1,31 @@
 import unittest
 from datetime import datetime
-from richkit.retrieve import whois as wh
+from richkit.retrieve import whois
 
 
 class WhoisTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.domains = ["www.google.com", "www.cloudflare.com"]
-
     # .dk domains give unknownTld exception !
-    @unittest.skip("No reason to skip however it fails on ci requires more investigation")
     def test_get_whois_info(self):
         # last updated field skipped since it could be None
-        for i in self.domains:
-            whois_info = wh.get_whois_info(i)
-            date = whois_info["d_expiration_date"]
-            registrar = whois_info["d_registrar"]
-            assert len(registrar) > 0
-            assert type(date) == datetime
+
+        d = "www.google.com"
+        w = whois.get_whois_info(d)
+        self.assertTrue(len(w['registrar']) > 0)
+        # .com uses "thin" WHOIS, so we get expiry from both registry
+        # and registrar;
+        self.assertTrue(len(w['expiration_date']) == 2)
+        self.assertIsInstance(w['expiration_date'][0], datetime)
+        self.assertIsInstance(w['expiration_date'][1], datetime)
+
+        d = "www.cloudflare.com"
+        w = whois.get_whois_info(d)
+        self.assertTrue('registrar' in w)
+        self.assertTrue(len(w['registrar']) > 0)
+        self.assertTrue('expiration_date' in w)
+        # .com uses "thin" WHOIS, so we get expiry from both registry
+        # and registrar, but they are equal here, so only one is returned;
+        self.assertIsInstance(w['expiration_date'], datetime)
 
 
 if __name__ == '__main__':
